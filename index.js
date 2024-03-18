63ea69a7ea308c9da0708480aa4ec8f80a25ec11
GUNNYTHATIPAMULA <139752490+GUNNYTHATIPAMULA@users.noreply.github.com>
2024-03-18 16:50:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,39 +11,59 @@ let resultbox=document.querySelector(".resultbox");
 let showbtn=document.querySelector(".showbtn");
 let allmarks=document.querySelector(".allmarks");
 
-
-
-// second
-
-let image1= document.querySelector(".images1");
-let mainimage1=document.querySelector(".mainimage1");
-let examarks1=document.querySelector(".examarks1");
-let announcements1=document.querySelector(".announcements1");
-let resultbox1=document.querySelector(".resultbox1");
-let showbtn1=document.querySelector(".showbtn1");
-let allmarks1=document.querySelector(".allmarks1");
-
 // alert("fdsj")
 
+let htno=document.querySelector(".htno");
+let Name=document.querySelector(".Name");
+let m1=document.querySelector(".m1");
+let phy=document.querySelector(".phy");
+let bee=document.querySelector(".bee");
+let c=document.querySelector(".c");
+let pass=document.querySelector(".pass");
+let studentimg=document.querySelector(".studentimg")
+
+
+function unhide(){
+  image.classList.toggle("hide");
+    one.classList.add("hide");
+    mainimage.classList.toggle("hide")
+}
+// 
 btn.addEventListener("click",page);
 function page(){
   if(username.value==="23K91A04N9" && password.value==="23K91A04N9"){
     console.log("baksjh")
-    image.classList.toggle("hide");
-    one.classList.add("hide");
-    mainimage.classList.toggle("hide")
-    
+    unhide();
+    take1();
+  }
+  else if(username.value==="23K91A04N5" && password.value==="23K91A04N5"){
+    unhide();
+    take2();
+  }
+  else if(username.value==="23K91A04K3" && password.value==="23K91A04K3"){
+    unhide();
+    take3();
+  }
+  else if(username.value==="23K91A04L1" && password.value==="23K91A04L1"){
+    unhide();
+    take4();
+  }
+  else if(username.value==="23K91A04N6" && password.value==="23K91A04N6"){
+    unhide();
+    take5();
+  }
+  else if(username.value==="23K91A04R1" && password.value==="23K91A04R1"){
+    unhide();
+    take6();
   }
   else if(username.value==="23K91A04M2" && password.value==="23K91A04M2"){
-    image1.classList.toggle("hide");
-    one.classList.add("hide");
-    mainimage.classList.toggle("hide")
+    unhide();
+    take7();
   }
   else{
     alert("Server Error!!!");
   }
 };
-// me
 
 examarks.addEventListener("click",()=>{
   announcements.classList.add("hide");
@@ -56,13 +76,166 @@ showbtn.addEventListener("click",()=>{
 
 
 // second
-examarks1.addEventListener("click",()=>{
-  announcements1.classList.add("hide");
-  resultbox1.classList.remove("hide");
-})
-showbtn1.addEventListener("click",()=>{
-  // allmarks.classList.("hide");
-  allmarks1.style.display="flex";
-});
 
 
+
+const student1details ={
+  name:"THATIPAMULA GUNNY",
+rollno:"23K91A04N9",
+m1:56,
+phy:50,
+bee:60,
+c:65,
+image:"images/studentPhoto.jpeg"
+
+};
+const student2details={
+  name:"SYED ARIF BABA  ",
+rollno:"23K91A04N5",
+m1:56,
+phy:50,
+bee:60,
+c:65,
+pass:"pass",
+image:"images/studentPhoto (2).jpeg"
+};
+const student3details={
+  name:"PEDDI GOPICHAND",
+rollno:"23K91A04K3",
+m1:32,
+phy:40,
+bee:20,
+c:45,
+pass:"Fail",
+image:"images/studentPhoto (3).jpeg",
+
+};
+const student4details={
+  name:"POSHABOINA VEERENDRA ",
+rollno:"23K91A04L1",
+m1:56,
+phy:50,
+bee:60,
+c:65,
+pass:"pass",
+image:"images/studentPhoto (4).jpeg",
+
+};
+const student5details={
+  name:"	TAAK ANSH  ",
+rollno:"23K91A04N6",
+m1:30,
+phy:43,
+bee:45,
+c:50,
+pass:"Fail",
+image:"images/studentPhoto (5).jpeg",
+
+};
+const student6details={
+  name:"	YARRANKI VAMSI  ",
+rollno:"23K91A04R1",
+m1:56,
+phy:50,
+bee:60,
+c:65,
+pass:"pass",
+image:"images/studentPhoto (6).jpeg",
+
+};
+const student7details={
+  name:"SHAIK AFSAR ",
+rollno:"23K91A04M2",
+m1:42,
+phy:50,
+bee:60,
+c:65,
+pass:"pass",
+image:"images/studentPhoto (1).jpeg"
+
+};
+// console.log(student1details)
+function take1(){
+  // alert("jhi")
+  Name.innerText=student1details.name;
+  htno.innerText=student1details.rollno;
+  m1.innerText=student1details.m1;
+  phy.innerText=student1details.phy;
+  bee.innerText=student1details.bee;
+  c.innerText=student1details.c; 
+  pass.innerText=student1details.pass;
+  studentimg.src=student1details.image;
+
+};
+function take2(){
+  // alert("jhi")
+  Name.innerText=student2details.name;
+  htno.innerText=student2details.rollno;
+  m1.innerText=student2details.m1;
+  phy.innerText=student2details.phy;
+  bee.innerText=student2details.bee;
+  c.innerText=student2details.c;
+  pass.innerText=student2details.pass;
+  studentimg.src=student2details.image;
+
+};
+function take3(){
+  // alert("jhi")
+  Name.innerText=student3details.name;
+  htno.innerText=student3details.rollno;
+  m1.innerText=student3details.m1;
+  phy.innerText=student3details.phy;
+  bee.innerText=student3details.bee;
+  c.innerText=student3details.c;
+  pass.innerText=student3details.pass;
+  studentimg.src=student3details.image;
+
+
+};
+function take4(){
+  // alert("jhi")
+  Name.innerText=student4details.name;
+  htno.innerText=student4details.rollno;
+  m1.innerText=student4details.m1;
+  phy.innerText=student4details.phy;
+  bee.innerText=student4details.bee;
+  c.innerText=student4details.c;
+  pass.innerText=student4details.pass;
+  studentimg.src=student4details.image;
+
+};
+function take5(){
+  // alert("jhi")
+  Name.innerText=student5details.name;
+  htno.innerText=student5details.rollno;
+  m1.innerText=student5details.m1;
+  phy.innerText=student5details.phy;
+  bee.innerText=student5details.bee;
+  c.innerText=student5details.c;
+  pass.innerText=student5details.pass;
+  studentimg.src=student5details.image;
+
+};
+function take6(){
+  // alert("jhi")
+  Name.innerText=student6details.name;
+  htno.innerText=student6details.rollno;
+  m1.innerText=student6details.m1;
+  phy.innerText=student6details.phy;
+  bee.innerText=student6details.bee;
+  c.innerText=student6details.c;
+  pass.innerText=student6details.pass;
+  studentimg.src=student7details.image;
+
+};
+function take7(){
+  Name.innerText=student7details.name;
+  htno.innerText=student7details.rollno;
+  m1.innerText=student7details.m1;
+  phy.innerText=student7details.phy;
+  bee.innerText=student7details.bee;
+  c.innerText=student7details.c;
+  pass.innerText=student7details.pass;
+  studentimg.src=student7details.image;
+
+};
